657ea765319403fd8db0bc15d4e1081afc8bc485
Guilherme Rodrigues Salerno <Guilherme Rodrigues Salerno>
2015-11-29 12:03:02 -0800

    Integrating Express for REST API

--- Node/home.js
+++ Node/home.js
@@ -1,15 +1,13 @@
 // Node application start up
 
 var port = process.env.PORT || 3000;
-var http = require('http');
-var url = require('url');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function (request, response) {
-	path = url.parse(request.url).pathname;
-	response.writeHead(200, { "Content-Type": "text/plain"});
-	response.end(path + "\n");
+app.get('/listings/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
 });
 
-server.listen(port);
+app.listen(port);
 
 console.log("Server running at http://127.0.0.1:" + port);
